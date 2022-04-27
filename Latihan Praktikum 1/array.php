81ae0dcbf251d8a199121b7eaaa5f2229e775713
rhizmaelliza <100072038+rhizmaelliza@users.noreply.github.com>
2022-04-27 20:28:41 +0700

    Add files via upload

---
+++ Latihan Praktikum 1/array.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+	<title>Pemrograman PHP dengan Array</title>
+</head>
+<body>
+<?php
+//penulisan array dapat dibuat seperti berikut
+$nama[] = "Rhizma";
+$nama[] = "Elliza";
+$nama[] = "Putri";
+echo $nama[1] . $nama[2] . $nama[0];
+echo "<br>";
+?>
+</body>
+</html>
---
+++ Latihan Praktikum 1/mod_1.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+	<title> Variabel </title>
+</head>
+<body>
+<?php
+$nilai_1 = 10;
+$nilai_2 = 3;
+$nilai_3 = 2 * $nilai_1 + 8 * $nilai_2;
+echo "nilai = ", $nilai_3;
+echo "<br>";
+$jumlah = $nilai_1 + $nilai_2;
+echo "hasil dari $nilai_1 + $nilai_2 adalah : $jumlah"; echo "<br><br>";
+echo "\"Nama : Rhizma Elliza Putri\" <br>";
+echo "NIM : 20082010120";
+?>
+</body>
+</html>
